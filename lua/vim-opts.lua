e0d076b45fe14ffe3abbfde66e33b3490a19de5c
Joshua ullrich <adabam2003@outlook.de>
2022-11-12 14:02:39 +0100

    add cursorline

--- lua/vim-opts.lua
+++ lua/vim-opts.lua
@@ -2,6 +2,7 @@ vim.opt.timeoutlen = 0
 vim.wo.number = true
 vim.g.encoding = 'UTF-8'
 vim.opt.termguicolors = true
+vim.o.cursorline = true
 vim.g.nobackup = true
 vim.g.nowritebackup = true
 vim.opt.mouse = 'a'
