9beb0f9f738dd29afac2fbee86734e3009e639b3
Joshua ullrich <adabam2003@outlook.de>
2022-11-13 15:17:55 +0100

    dashboard

--- plugin/dashboardmenu.lua
+++ plugin/dashboardmenu.lua
@@ -3,4 +3,17 @@ if not ok then
 	return
 end
 
-alpha.setup(require('alpha.themes.dashboard').config)
+local dashboard = require('alpha.themes.dashboard')
+
+
+dashboard.section.buttons.val = {
+	dashboard.button('e', ' New file'),
+	dashboard.button('SPC f f', ' Find file'),
+	dashboard.button('SPC f h', ' Recent files'),
+	dashboard.button('SPC f p', 'ﴬ Projects'),
+	dashboard.button('SPC N c', '漣Settings')
+}
+
+
+alpha.setup(dashboard.opts)
+
