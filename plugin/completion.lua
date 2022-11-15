b4615061a8d285c95d3a86f98c6238e5f526f8bb
Joshua ullrich <adabam2003@outlook.de>
2022-11-15 13:44:22 +0100

    completion icons

--- plugin/completion.lua
+++ plugin/completion.lua
@@ -14,12 +14,45 @@ cmp.setup {
         ['<cr>'] = cmp.mapping.confirm({select = true})
     },
     window = {
-        completion = cmp.config.window.bordered(),
-        documentation = cmp.config.window.bordered()
+        completion = cmp.config.window.bordered{
+        	scrolloff = 1,
+        },
+        documentation = cmp.config.window.bordered{
+        }
     },
 	sources = cmp.config.sources({
 		{name = "nvim_lsp"}, {name = "luasnip"}
-    }, {{name = "buffer"}})
+    }, {{name = "buffer"}}),
+    formatting = {
+		format = function(entry, vim_item)
+			local icons = {
+				Keyword = '',
+				Text = '',
+				Snippet = '',
+				Function = '',
+				Class = '',
+				Interface = '',
+				Module = '',
+				Struct = '',
+				Enum = '',
+				EnumMember = '',
+				Variable = '',
+				Method = '',
+				Field = '',
+				Property = '',
+				Constructor = '',
+				Reference = ''
+			}
+
+			for k, v in pairs(icons) do
+				if vim_item.kind == k then
+					vim_item.kind = v .. ' ' .. vim_item.kind
+					break
+				end
+			end
+			return vim_item
+		end
+    }
 }
 
 cmp.setup.cmdline('/', {
@@ -33,4 +66,3 @@ cmp.setup.cmdline(":", {
 })
 
 cmp.event:on('confirm_done', require("nvim-autopairs.completion.cmp").on_confirm_done())
-
