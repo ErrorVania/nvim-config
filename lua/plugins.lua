51e1cdef71bb9bab3b38c202ca477ff4d4b08bb7
Joshua <ullrich.joshua@protonmail.com>
2022-11-22 09:29:51 +0100

    profiling enabled

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -13,7 +13,11 @@ packer.init {
 		open_fn = function()
 			return require('packer.util').float { border = 'rounded' }
 		end
-	}
+	},
+	profile = {
+      enable = true,
+      threshold = 1 -- the amount in ms that a plugin's load time must be over for it to be included in the profile
+    }
 }
 
 packer.startup(function(use)
