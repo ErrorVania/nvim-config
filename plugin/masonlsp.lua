80b4081f4a4cce3eb71aaf6ca986a0c9f006ea67
Joshua ullrich <adabam2003@outlook.de>
2022-11-23 03:44:48 +0100

    remove comment

--- plugin/masonlsp.lua
+++ plugin/masonlsp.lua
@@ -17,18 +17,6 @@ if colorok then
 	colors = colors.get_palette()
 
 	local TelescopeColor = {
-		-- TelescopeMatching = { fg = colors.flamingo },
-		-- TelescopeSelection = { fg = colors.text, bg = colors.surface0, bold = true },
-		-- TelescopePromptPrefix = { bg = colors.surface0 },
-		-- TelescopePromptNormal = { bg = colors.surface0 },
-		-- TelescopeResultsNormal = { bg = colors.mantle },
-		-- TelescopePreviewNormal = { bg = colors.mantle },
-		-- TelescopePromptBorder = { bg = colors.surface0, fg = colors.surface0 },
-		-- TelescopeResultsBorder = { bg = colors.mantle, fg = colors.mantle },
-		-- TelescopePreviewBorder = { bg = colors.mantle, fg = colors.mantle },
-		-- TelescopePromptTitle = { bg = colors.pink, fg = colors.mantle },
-		-- TelescopeResultsTitle = { fg = colors.mantle },
-		-- TelescopePreviewTitle = { bg = colors.green, fg = colors.mantle },
 		MasonHeader = {bg = colors.green, fg = colors.mantle }
 	}
 
