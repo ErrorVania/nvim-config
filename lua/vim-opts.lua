3f2aba95aa12320bda1042a74ffa96ffd939ff02
Joshua ullrich <adabam2003@outlook.de>
2023-01-07 18:58:06 +0100

    eob

--- lua/vim-opts.lua
+++ lua/vim-opts.lua
@@ -12,6 +12,7 @@ vim.opt.expandtab = false
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.pumheight = 10
+vim.opt.fillchars = 'eob: '
 
 vim.cmd("set completeopt=menu,menuone,noselect")
 vim.cmd("set noet ci pi sts=0 sw=4 ts=4")
