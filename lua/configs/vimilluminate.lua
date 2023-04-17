ba7984e328a7f5f6cfe4505d0f68f70bc31a1134
Joshua <ullrich.joshua@protonmail.com>
2023-04-17 11:10:18 +0200

    use import in vimilluminate

--- lua/configs/vimilluminate.lua
+++ lua/configs/vimilluminate.lua
@@ -1,21 +1,18 @@
-local ok, illuminate = pcall(require, 'illuminate')
-if not ok then
-	return
-end
-
-illuminate.configure {
-	providers = {
-		'lsp',
-		'treesitter'
+import('illuminate', function(i)
+	i.configure {
+		providers = {
+			'lsp',
+			'treesitter'
+		}
 	}
-}
 
-local IlluminateColor = {
-	IlluminatedWordRead = { link = 'Visual' },
-	IlluminatedWordWrite = { link = 'Visual' },
-	IlluminatedWordText = { link = 'Visual' }
-}
+	local IlluminateColor = {
+		IlluminatedWordRead = { link = 'Visual' },
+		IlluminatedWordWrite = { link = 'Visual' },
+		IlluminatedWordText = { link = 'Visual' }
+	}
 
-for hl, col in pairs(IlluminateColor) do
-	vim.api.nvim_set_hl(0, hl, col)
-end
+	for hl, col in pairs(IlluminateColor) do
+		vim.api.nvim_set_hl(0, hl, col)
+	end
+end)
