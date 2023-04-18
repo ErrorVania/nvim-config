7ac74559eaf3619bfb82a8d02556847270dff793
Joshua <ullrich.joshua@protonmail.com>
2023-04-18 14:08:40 +0200

    move import function into own file

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,28 +1,3 @@
-function import(modules, callback)
-	if modules ~= nil then
-		local result = nil
-		if type(modules) == 'table' then
-			result = {}
-			for k = 1, #modules do
-				local ok, m = pcall(require, modules[k])
-				if not ok then
-					modules[k] = nil
-				else
-					result[modules[k]] = m
-				end
-			end
-		elseif type(modules) == 'string' then
-			local ok, m = pcall(require, modules)
-			if ok then
-				result = m
-			end
-		end
-		if callback ~= nil and result ~= nil then
-			callback(result)
-		end
-	end
-end
-
 return {
 	{
 		'lewis6991/impatient.nvim',
