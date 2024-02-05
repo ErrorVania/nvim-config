28ea5e339e431fcbc93d85d072f6d42dfc090455
Joshua <ullrich.joshua@protonmail.com>
2024-02-05 10:55:16 +0100

    focus options inti init.lua

--- init.lua
+++ init.lua
@@ -27,18 +27,104 @@ end
 local lazy_bootstrapped = bootstrap_lazy()
 
 local lazy_ok, lazy = pcall(require, 'lazy')
-if lazy_ok then
-	vim.g.mapleader = ' '
-	import'impatient'
-	lazy.setup({ spec = 'plugins', opts = { install = { colorscheme = 'catppuccin-frappe' } } })
-	if not lazy_bootstrapped and lazy_ok then
-		local autocmds = require('autocmds')
-		for _, cmd in pairs(autocmds) do
-			vim.api.nvim_create_autocmd(cmd[1],cmd[2])
-		end
-		require('vim-opts')
-		require('lspconfigs')
-		require('keymaps')
-	end
+if not lazy_ok then
+	return
 end
+
+vim.g.mapleader = ' '
+import'impatient'
+lazy.setup({ spec = 'plugins', opts = { install = { colorscheme = 'catppuccin-frappe' } } })
+
+if lazy_bootstrapped then
+	return
+end
+
+-- autocommands
+local autocmds = require 'autocmds'
+autocmds.define_autocmds{
+	{
+		'FileType',
+		{
+			pattern = { 'help', 'checkhealth', 'OverseerList', 'ImportManager' },
+			command = [[nnoremap <buffer><silent> q :close<CR>]]
+		}
+	},
+	{
+		'FileType',
+		{
+			pattern = { 'alpha' },
+			command = [[nnoremap <buffer><silent> q :quit<CR>]]
+		}
+	},
+	{
+		{ 'VimEnter', 'DirChanged' },
+		{ callback = function() vim.schedule(autocmds.check_git_repo) end }
+	},
+	{
+		{ 'VimEnter' },
+		{ callback = function() vim.schedule(autocmds.is_tmux_sess) end }
+	},
+	{
+		{ 'VimEnter', 'FileType' },
+		{
+			callback = function()
+				local blacklist = {
+					'mason',
+					'packer'
+				}
+				for _, value in ipairs(blacklist) do
+					if vim.bo.filetype == value then
+						return
+					end
+				end
+				vim.schedule(autocmds.is_editing)
+			end,
+			pattern = '*'
+		}
+	}
+}
+
+-- vim options
+vim.opt.timeoutlen = 0
+vim.wo.number = true
+vim.g.encoding = 'UTF-8'
+vim.opt.termguicolors = true
+vim.o.cursorline = true
+vim.g.nobackup = true
+vim.g.nowritebackup = true
+vim.opt.mouse = 'a'
+vim.cmd("filetype plugin indent on")
+vim.opt.autoindent = true
+vim.opt.expandtab = false
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.pumheight = 10
+vim.opt.fillchars = 'eob: '
+vim.cmd("set completeopt=menu,menuone,noselect")
+
+require('configs.theme').load_theme()
+
+-- configure lsp
+require('lspconfigs')
+
+-- keymaps
+vim.keymap.set('n', '<Space>', '', {}) --leader
+
+local function nmap(mode, keys, command)
+	return vim.keymap.set(mode, keys, command, { noremap = true })
+end
+
+nmap('n', '<C-s>', '<cmd>w<cr>')
+nmap('n', '<S-h>', '<cmd>BufferLineCyclePrev<cr>')
+nmap('n', '<S-l>', '<cmd>BufferLineCycleNext<cr>')
+
+nmap('n', '<S-k>', vim.lsp.buf.hover)
+nmap('n', 'gd', vim.lsp.buf.declaration)
+nmap('n', 'gD', vim.lsp.buf.definition)
+
+
+nmap('n', '<C-h>', '<C-W>h')
+nmap('n', '<C-j>', '<C-W>j')
+nmap('n', '<C-k>', '<C-W>k')
+nmap('n', '<C-l>', '<C-W>l')
 
--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,4 +1,6 @@
-local function check_git_repo()
+local M = {}
+
+M.check_git_repo = function()
 	local f = vim.fs.find('.git', { type = 'directory' })
 	if f ~= {} then
 		vim.api.nvim_exec_autocmds('User', { pattern = 'InGitRepo' })
@@ -6,61 +8,24 @@ local function check_git_repo()
 	end
 end
 
-local function is_tmux_sess()
+M.is_tmux_sess = function()
 	if os.getenv('TMUX') then
 		vim.api.nvim_exec_autocmds('User', { pattern = 'IsTmuxSession' })
 		return true
 	end
 end
 
-local function is_editing()
+M.is_editing = function()
 	if vim.fn.argc() > 0 or vim.o.insertmode or vim.o.modifiable then
 		vim.api.nvim_exec_autocmds('User', { pattern = 'IsEditing' })
 		return true
 	end
 end
 
-local autocommands = {
-	{
-		'FileType',
-		{
-			pattern = { 'help', 'checkhealth', 'OverseerList', 'ImportManager' },
-			command = [[nnoremap <buffer><silent> q :close<CR>]]
-		}
-	},
-	{
-		'FileType',
-		{
-			pattern = { 'alpha' },
-			command = [[nnoremap <buffer><silent> q :quit<CR>]]
-		}
-	},
-	{
-		{ 'VimEnter', 'DirChanged' },
-		{ callback = function() vim.schedule(check_git_repo) end }
-	},
-	{
-		{ 'VimEnter' },
-		{ callback = function() vim.schedule(is_tmux_sess) end }
-	},
-	{
-		{ 'VimEnter', 'FileType' },
-		{
-			callback = function()
-				local blacklist = {
-					'mason',
-					'packer'
-				}
-				for _, value in ipairs(blacklist) do
-					if vim.bo.filetype == value then
-						return
-					end
-				end
-				vim.schedule(is_editing)
-			end,
-			pattern = '*'
-		}
-	}
-}
+M.define_autocmds = function(t)
+	for _, cmd in pairs(t) do
+		vim.api.nvim_create_autocmd(cmd[1],cmd[2])
+	end
+end
 
-return autocommands
+return M
